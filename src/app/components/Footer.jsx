78966c72e198369900386dd4cb9d88ba7d5010a2
JoseALopezP <jalp42@live.com>
2023-12-21 05:00:39 -0300

    Added header and bottom bouquets

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,8 +1,15 @@
+import Image from 'next/image'
 import styles from './Footer.module.css'
 
 export function Footer () {
     return (
         <section className={`${styles.footerBlock}`}>
+            <Image
+                src="/images/bouquets/bottom.png"
+                width={887}
+                height={267}
+                alt="Image"
+            />
         </section>
     )
 }
