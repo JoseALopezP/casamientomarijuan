05f8fd989a72ef79362213e2a6094a81ba7cdd03
JoseALopezP <jalp42@live.com>
2023-12-22 12:42:58 -0300

    Finished mobile version

--- src/app/components/Confirmation.jsx
+++ src/app/components/Confirmation.jsx
@@ -1,11 +1,25 @@
 import styles from './Confirmation.module.css'
+import Image from 'next/image'
 
 export function Confirmation () {
     return (
+        <>
+        <Image
+            className={`${styles.bouquet4}`}
+            src="/images/bouquets/side4.png"
+            width={284}
+            height={498}
+            alt="Image"
+        />
         <section className={`${styles.confirmationBlock}`}>
-            <h3>CONFIRMACION</h3>
-            <p>Por favor confirma tu asistencia antes de:</p>
-            <button type='button'>CONFIRMAR ASISTENCIA</button>
+            <svg width="60px" height="60px" viewBox="0 0 24 24" fill="none" className={`${styles.confirmationTick}`}>
+                <path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path opacity="0.34" d="M7.75 11.9999L10.58 14.8299L16.25 9.16992" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <h3 className={`${styles.confirmationTitle}`}>CONFIRMACION</h3>
+            <p className={`${styles.confirmationText}`}>POR FAVOR CONFIRMAR TU ASISTENCIA ANTES DEL 02 DE MARZO</p>
+            <button type='button' className={`${styles.confirmationButton}`}>CONFIRMAR ASISTENCIA</button>
         </section>
+        </>
     )
 }
