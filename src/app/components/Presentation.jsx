82013b308661efea7ea90607eeefdc4f8c09d3d6
JoseALopezP <jalp42@live.com>
2023-12-30 22:13:50 -0300

    Solved build issues

--- src/app/components/Presentation.jsx
+++ src/app/components/Presentation.jsx
@@ -6,8 +6,7 @@ export function Presentation () {
         <section className={`${styles.presentationSection}`}>
             <div className={`${styles.presentationBlock}`}>
                 <h1  className={`${playfair.className} antialiased`}>M |&nbsp; J</h1>
-                <p className={`${styles.quote}`}>"La medida del amor es amar sin medida"
-                </p>
+                <p className={`${styles.presentationQuote}`}>&quot;La medida del amor es amar sin medida&quot;</p>
                 <span>¡NOS CASAMOS!</span>
             </div>
         </section>
