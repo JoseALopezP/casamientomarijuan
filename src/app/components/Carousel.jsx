f0a741772f4727bc343aa8c4cefb398da1d30d0e
JoseALopezP <jalp42@live.com>
2024-02-12 16:51:52 -0300

    decreased original photo size

--- src/app/components/Carousel.jsx
+++ src/app/components/Carousel.jsx
@@ -11,8 +11,8 @@ export function Carousel () {
                 {Array.from(Array(12).keys()).map((el) =>{
                     return(<Image
                         src={'/images/photoshoot/' +(el+1)+'.png'}
-                        width={1280}
-                        height={720}
+                        width={1920}
+                        height={1080}
                         alt="Image"
                         key={el}
                     />)    
