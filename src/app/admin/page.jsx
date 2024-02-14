c32d234b4967cf61025d7d6bb1ff70f4a263ba73
JoseALopezP <jalp42@live.com>
2024-02-13 23:08:54 -0300

    solved component navbar

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -2,7 +2,7 @@
 import { useEffect } from "react";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
-import { NavBar } from "./components/navBar";
+import { NavBar } from "./components/NavBar";
 
 export default function Admin() {
     const { user } = useAuthContext()
