528641b307010cfef6c309d22961a07d3337fa3f
JoseALopezP <jalp42@live.com>
2024-03-06 21:56:20 -0300

    changed after schedule

--- src/app/components/Information.jsx
+++ src/app/components/Information.jsx
@@ -2,7 +2,7 @@ import { playfair } from '../ui/fonts'
 import styles from './Information.module.css'
 import Link from 'next/link'
 
-export function Information () {
+export function Information ({dinner}) {
     return (
         <section className={`${styles.informationSection}`}>
             <div className={`${styles.informationBlock}`}>
@@ -19,7 +19,11 @@ export function Information () {
             <p className={`${styles.dateInfoPlace}`}>PARROQUIA SANTA LUCÍA</p>
             <p className={`${styles.dateInfoUbi}`}><i>Gral. Paz y Ramón Franco</i></p>
             <Link target="_blank" href="https://maps.app.goo.gl/12mcXTTXgNSMR94T6" className={`${styles.dateInfoUbiTag}`}>VER UBICACION</Link>
+            {dinner=='ccena'?
             <h5 className={`${styles.dateInfoHour}`}>21:00 HS</h5>
+            :
+            <h5 className={`${styles.dateInfoHour}`}>00:00 HS</h5>
+            }
             <h4 className={`${styles.dateInfoTitle}`}>RECEPCIÓN</h4>
             <p className={`${styles.dateInfoPlace}`}>SALÓN GALANA EVENTOS</p>
             <p className={`${styles.dateInfoUbi}`}><i>Balcarce 198</i></p>
